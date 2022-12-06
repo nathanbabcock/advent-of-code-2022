704859ece62b458f2eb75737a56d34fd8fec9580
nbabcock <nathan.r.babcock@gmail.com>
2022-12-06 10:28:55 -0600

    feat: solve day 6 part 2 :star:

--- days/6/6-1.ts
+++ days/6/6-1.ts
@@ -1,15 +1,15 @@
 import chalk from 'chalk'
 import { readFileSync } from 'fs'
 
-function bufStart(buf: string, start = 0): number {
-  const maybeMarker = buf.slice(start, start + 4)
+function bufStart(buf: string, markerLen = 4, start = 0): number {
+  const maybeMarker = buf.slice(start, start + markerLen)
   const chars = new Set([...maybeMarker.split('')])
   // console.log({ buf, start, maybeMarker, chars })
-  if (chars.size === 4) return start + 4
-  else return bufStart(buf, start + 1)
+  if (chars.size === markerLen) return start + markerLen
+  else return bufStart(buf, markerLen, start + 1)
 }
 
-console.log(chalk.bgGreen(' Test input '))
+console.log(chalk.bgGreen(' Part 1 test input '))
 console.log(bufStart('mjqjpqmgbljsphdztnvjfqwrcgsmlb'))
 console.log(bufStart('bvwbjplbgvbhsrlpgdmjqwftvncz'))
 console.log(bufStart('nppdvjthqldpwncqszvftbrmjlhg'))
@@ -18,5 +18,19 @@ console.log(bufStart('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'))
 console.log()
 
 const realInput = readFileSync('days/6/6.txt').toString()
-console.log(chalk.bgGreen(' Real input '))
+console.log(chalk.bgGreen(' Part 1 real input '))
 console.log(bufStart(realInput))
+console.log()
+
+console.log(chalk.bgGreen(' Part 2 test input '))
+console.log(bufStart('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 14))
+console.log(bufStart('bvwbjplbgvbhsrlpgdmjqwftvncz', 14))
+console.log(bufStart('nppdvjthqldpwncqszvftbrmjlhg', 14))
+console.log(bufStart('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 14))
+console.log(bufStart('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 14))
+console.log()
+
+console.log(chalk.bgGreen(' Part 2 real input '))
+console.log(bufStart(realInput, 14))
+console.log()
+console.log('⭐⭐')
