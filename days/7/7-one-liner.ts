520055d27945eff9e6b3d79224b69ced56eec017
nbabcock <nathan.r.babcock@gmail.com>
2022-12-07 17:40:24 -0600

    refactor: inline entire day 6 part 1 & 2 solution

--- days/7/7-one-liner.ts
+++ days/7/7-one-liner.ts
@@ -1,24 +1,3 @@
-
-
-const addEmptyDirs = (dirs, allDirs = dirs) =>
-  dirs.length === 0
-    ? allDirs // base case
-    : ((dir, remainingDirs) => // loop over dirs
-      addEmptyDirs(remainingDirs, // (top-level) recursion
-        // get all subpaths
-        ((subPathFn) => subPathFn(dir.path, allDirs, subPathFn)) // insanity; a function that returns the invocation of its parameter *on itself*
-          ((subPath, dirs, subPathFn) =>
-            subPath.length === 0
-              ? dirs // base case
-              : dirs.some(dir => dir.path.every((p, i) => p === subPath[i]) && dir.path.length === subPath.length)
-                ? subPathFn(subPath.slice(0, -1), dirs, subPathFn) // already have a dir for this path
-                : subPathFn(subPath.slice(0, -1), [...dirs, { path: [...subPath], size: 0 }], subPathFn) // missing dir; add one
-          )
-      )
-    )(dirs[0], dirs.slice(1))
-
-  ////
-  ;
 ((input) =>
   ((files) =>
     ((dirs) => ({
@@ -35,31 +14,46 @@ const addEmptyDirs = (dirs, allDirs = dirs) =>
       }))
         .filter(dir => dir.size >= 30_000_000 - (70_000_000 - files.reduce((acc, file) => acc + file.size, 0)))
         .sort((a, b) => a.size - b.size)[0].size,
-    }))(addEmptyDirs(
-      //old
-      // getDirsFunctional(files)
-
-      //new
-      ((getDirsFunctional) => getDirsFunctional(files, [], getDirsFunctional))(
-        (files, dirs = [], getDirsFunctional) =>
-          files.length === 0
-            ? dirs // base case
-            : ((file, remainingFiles) =>
-              getDirsFunctional(
-                remainingFiles,
-                ((existingDir) => [ // add file size to dir
-                  { path: existingDir.path, size: existingDir.size + file.size },
-                  ...dirs.filter(dir => dir !== existingDir),
-                ])( // find existing dir or create it
-                  dirs.find(dir =>
-                    dir.path.every((p, i) =>
-                      p === file.path[i]
-                    ) && dir.path.length === file.path.length
-                  ) ?? { path: [...file.path], size: 0 }),
-                getDirsFunctional)
-            )(files[0], files.slice(1))
+    }))(
+      ((addEmptyDirs, dirs) => addEmptyDirs(dirs, dirs, addEmptyDirs))(
+        (dirs, allDirs = dirs, addEmptyDirs) =>
+          dirs.length === 0
+            ? allDirs // base case
+            : ((dir, remainingDirs) => // loop over dirs
+              addEmptyDirs(remainingDirs, // (top-level) recursion
+                // get all subpaths
+                ((subPathFn) => subPathFn(dir.path, allDirs, subPathFn)) // insanity; a function that returns the invocation of its parameter *on itself*
+                  ((subPath, dirs, subPathFn) =>
+                    subPath.length === 0
+                      ? dirs // base case
+                      : dirs.some(dir => dir.path.every((p, i) => p === subPath[i]) && dir.path.length === subPath.length)
+                        ? subPathFn(subPath.slice(0, -1), dirs, subPathFn) // already have a dir for this path
+                        : subPathFn(subPath.slice(0, -1), [...dirs, { path: [...subPath], size: 0 }], subPathFn) // missing dir; add one
+                  ),
+                addEmptyDirs
+              )
+            )(dirs[0], dirs.slice(1)),
+        ((getDirsFunctional) => getDirsFunctional(files, [], getDirsFunctional))(
+          (files, dirs = [], getDirsFunctional) =>
+            files.length === 0
+              ? dirs // base case
+              : ((file, remainingFiles) =>
+                getDirsFunctional(
+                  remainingFiles,
+                  ((existingDir) => [ // add file size to dir
+                    { path: existingDir.path, size: existingDir.size + file.size },
+                    ...dirs.filter(dir => dir !== existingDir),
+                  ])( // find existing dir or create it
+                    dirs.find(dir =>
+                      dir.path.every((p, i) =>
+                        p === file.path[i]
+                      ) && dir.path.length === file.path.length
+                    ) ?? { path: [...file.path], size: 0 }),
+                  getDirsFunctional)
+              )(files[0], files.slice(1))
+        )
       )
-    ))
+    )
   )(
     ((getFilesFunctional, lines) => getFilesFunctional(lines, [], [], getFilesFunctional))
       ((lines, files = [], curPath = [], getFilesFunctional) =>
@@ -76,5 +70,3 @@ const addEmptyDirs = (dirs, allDirs = dirs) =>
               : getFilesFunctional(otherLines, files, curPath, getFilesFunctional))(lines[0], lines.slice(1)) // ...(head(lines), tail(lines))
         , input)
   ))(document.body.innerText.split(/\r?\n/)) // <- input goes here ;)
-
-
